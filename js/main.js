a80c41c15cb9bd784c161438bb923751ebe58e0f
Johannes <johannesotto0903@gmx.de>
2022-02-03 09:51:30 +0100

    moin

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function zufaelligeKarte (){
+    let myArrary = [
+        "Kreuz7",
+        "Kreuz8",
+        "Kreuz9",
+        "Kreuz10",
+        "Kreuz11",
+        "Kreuz12",
+        "Kreuz13",
+        "Kreuz14",
+
+
+    ]
+
+}
