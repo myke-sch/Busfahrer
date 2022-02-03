95c751672e40bd7fa05d979553df890de502a4da
BennoAlter <b.hartleib23@gmail.com>
2022-02-03 10:08:15 +0100

    test

--- js/main.js
+++ js/main.js
@@ -7,4 +7,13 @@ console.log(karo_karte);
 
 karten_bilder = document.getElementById("karo_bilder");
 
-karten_bilder.src = 
+karten_bilder.src = karo_karte;
+
+let pik_deck = ["pik7", "pik8", "pik9", "pik10", "pikB", "pikD", "pikK", "pikA"];
+pik_karte = pik_deck[Math.floor(Math.random() * (pik_deck.length - 0)) + 0]
+
+console.log(pik_karte);
+
+karten_bilder = document.getElementById("pik_bilder");
+
+karten_bilder.src = pik_karte;
