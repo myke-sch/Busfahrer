51bf0447016311266cf00a3bc440f40f1b66f866
scheibmi <mika.scheibler@online.de>
2022-02-03 10:06:19 +0100

    Test

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 //B = Bube, D = Dame, K = König, A = Ass
 
+//let karo_deck = ["karo7", "karo8","karo9","karo10", "karoB", "karoD", "karoK", "karoA"];
 let karo_deck = ["karo7", "karo8","karo9","karo10", "karoB", "karoD", "karoK", "karoA"];
 karo_karte = karo_deck[Math.floor(Math.random() * (karo_deck.length - 0)) + 0];
 
 console.log(karo_karte);
 
-karten_bilder = document.getElementById("karo_bilder");
+karten_bilder = document.getElementsByClassName("image1");
 
-karten_bilder.src = 
+karten_bilder.src = "img/karo7.png";
