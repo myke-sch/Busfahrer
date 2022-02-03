a89b817487099557444de368f8c91385522499d6
leaholbein <lea.holbein@gmail.com>
2022-02-03 10:08:02 +0100

    Herz function

--- js/main.js
+++ js/main.js
@@ -12,3 +12,11 @@ function zufaelligeKarte(){
     let randomItem = herzArray[Math.floor(Math.random()*myArray.length)];
     document.getElementById("kartendeck").innerHTML = zufaelligeKarte;
 }
+let karo_deck = ["herz7", "herz8","herz9","herz10", "herzB", "herzD", "herzK", "herzA"];
+karo_karte = karo_deck[Math.floor(Math.random() * (karo_deck.length - 0)) + 0];
+
+console.log(karo_karte);
+
+karten_bilder = document.getElementById("karo_bilder");
+
+karten_bilder.src =
